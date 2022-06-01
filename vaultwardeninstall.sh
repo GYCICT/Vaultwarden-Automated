09ae1df323eade8972b227e7888c1a1c1b79b9f5
George Dellas <89891088+gorgdel@users.noreply.github.com>
2022-06-02 08:49:29 +1000

    removed nginx + fixed vaultwarden

--- vaultwardeninstall.sh
+++ vaultwardeninstall.sh
@@ -34,22 +34,15 @@ apt-get -y install docker-ce docker-ce-cli containerd.io
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=no -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
-# Docker compose (https://docs.docker.com/compose/)
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-# Nginx Proxy Manager (https://nginxproxymanager.com/)
-# LetsEncrypt won't work due to port 80 not being exposed on this yml
-# To get letsencrypt to work, you will need to follow this guide. (https://github.com/gorgdel/Vaultwarden-Automated) 
-mkdir /opt/nginxproxymanager
-cd /opt/nginxproxymanager
-curl -L "https://raw.githubusercontent.com/GYCICT/Vaultwarden-Automated/master/docker-compose.yml" --output docker-compose.yml
-docker-compose up -d
-cd
-
 # Vaultwarden (https://github.com/dani-garcia/vaultwarden)
 docker pull vaultwarden/server:latest
-docker run -d --name vaultwarden --restart=always -v /vw-data/:/data/ vaultwarden/server:latest
-clear
+docker run -d --name vaultwarden \
+--restart=always \
+-e ROCKET_TLS='{certs="/ssl/cert.pem",key="/ssl/priv.key"}' \
+-v /ssl/:/ssl/ \
+-v /vw-data/:/data/ \
+-p 443:80 \
+vaultwarden/server:latest
+
 echo "Complete - Proceed with other steps."
 sleep 0.5 
